c710209d42804bbc21abc2cf13dd32db2b19212c
Alejandro Perez <alexandro9103@gmail.com>
2020-01-08 14:37:05 -0200

    colocando el Host en el app.listen

--- index.js
+++ index.js
@@ -76,6 +76,6 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 //Escuchar en el puerto
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`The server is running at: http://${host}:${port}`);
 })
