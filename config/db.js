d6f6359199d1a9ab0e5d4bb40d8cddba7ef64920
Alejandro Perez <alexandro9103@gmail.com>
2020-01-08 14:44:53 -0200

    Cambios en el Index.js y en db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const sequelize = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, proc
     host: process.env.BD_HOST,
     dialect: 'mysql',
     port: process.env.BD_PORT,
-    //operatorsAliases: false,
     define: {
         timestamps: false
     },
